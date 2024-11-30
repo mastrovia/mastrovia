c9fe441ca3bbdb2db777efd50fd3b1cf5e56e06d
Remin T Roy <86014784+remintroy@users.noreply.github.com>
2024-11-30 13:22:01 +0530

    feat(update): ui update

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,18 +1,32 @@
 import { motion, MotionProps } from "motion/react";
 import { ButtonHTMLAttributes, FC } from "react";
 
-type ButtonProps = MotionProps & ButtonHTMLAttributes<HTMLButtonElement>;
+type CustomProps = {
+  invertMode?: boolean;
+};
 
-const Button: FC<ButtonProps> = ({ children, ...props }) => {
+type ButtonProps = MotionProps & ButtonHTMLAttributes<HTMLButtonElement> & CustomProps;
+
+const Button: FC<ButtonProps> = ({ children, invertMode, ...props }) => {
   return (
     <motion.button
       {...props}
-      whileHover={{ scale: 1.1 }}
+      whileHover={{
+        // scale: 1.1,
+        // backgroundColor: !invertMode ? "var(--bg)" : "var(--primary)",
+        backgroundColor: "#00000000",
+        color: invertMode ? "var(--bg)" : "var(--primary)",
+        backdropFilter: "blur(5px)",
+      }}
+      transition={{ duration: 0.15 }}
       whileTap={{ scale: 0.95 }}
       style={{
         padding: "8px 16px",
         fontWeight: "bold",
         userSelect: "none",
+        backgroundColor: invertMode ? "var(--bg)" : "var(--primary)",
+        color: !invertMode ? "var(--bg)" : "var(--primary)",
+        border: `1px solid ${invertMode ? "var(--bg)" : "var(--primary)"}`,
         ...props?.style,
       }}
     >
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,4 +1,5 @@
 // import starAiIcon from "../assets/Star-ai-icon.png";
+// import arrow from "../assets/arrow.png";
 import { motion } from "motion/react";
 import Button from "../Components/Button";
 
@@ -11,6 +12,7 @@ export default function HomePage() {
 
       <section className="container mx-auto p-4 text-center  flex flex-col justify-center gap-7 h-[100vh] max-h-[600px] max-w-7xl relative">
         {/* <img src={starAiIcon} className="w-[50px] h-[50px] object-scale-down" alt="" /> */}
+        {/* <img src={arrow} alt="" className="absolute bottom-28 left-44"/> */}
         <h1 className="text-6xl lg:text-7xl xl:text-8xl font-bold leading-[1.1] title-aurora relative">
           Let's get your website ready
           <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.5 }} className="aurora">
@@ -22,7 +24,7 @@ export default function HomePage() {
         </h1>
         <p className="opacity-85">A web development company</p>
         <div>
-          <Button className="relative sm:w-auto bg-[var(--primary)] text-[var(--bg)] transition-colors hover:bg-transparent border hover:border-[var(--primary)] hover:text-[var(--primary)]">
+          <Button className="relative sm:w-auto">
             {/* <div className="absolute w-[300%] h-[1px] left-[-100%] top-0 bg-[var(--border)]" />
             <div className="absolute w-[300%] h-[1px] right-[-100%] bottom-0 bg-[var(--border)]" />
             <div className="absolute h-[300%] w-[1px] top-[-100%] left-0 bg-[var(--border)]" />
@@ -33,26 +35,36 @@ export default function HomePage() {
       </section>
 
       <section className="px-4 bg-[var(--primary)] text-[var(--bg)] flex flex-col gap-5 py-10">
-        <h1 className="text-xl font-bold text-center">Our services</h1>
+        {/* <h1 className="text-xl font-bold text-center">Our services</h1> */}
         <div className=" container mx-auto flex flex-col md:flex-row">
-          <div className="p-4">
-            <h3 className="font-bold">Design</h3>
+          <div className="p-4 flex flex-col gap-5 relative">
+            <h3 className="font-bold">We Design</h3>
+            {/* <img
+              src="/banners/design-illustration.jpg"
+              alt=""
+              className="z-0 mix-blend-darken grayscale hover:grayscale-0 opacity-20 absolute transition-all"
+            /> */}
             <p>
               Crafting visually stunning and user-friendly designs that captivate audiences and enhance user
               experiences. Our creative approach ensures your website reflects your brand’s identity while staying
               modern and intuitive.
             </p>
+            <div>
+              <Button invertMode>Know more</Button>
+            </div>
           </div>
-          <div className="p-4">
-            <h3 className="font-bold">Development</h3>
+          <div className="p-4 flex flex-col gap-5 relative">
+            <h3 className="font-bold">We Development</h3>
+            {/* <img src="/banners/development-illustration.jpg" alt="" className="mix-blend-darken" /> */}
             <p>
               Transforming innovative designs into fully functional websites using the latest technologies. Our
               development process focuses on performance, scalability, and user satisfaction to deliver a seamless
               digital experience.
             </p>
           </div>
-          <div className="p-4">
-            <h3 className="font-bold">Deployment</h3>
+          <div className="p-4 flex flex-col gap-5 relative">
+            <h3 className="font-bold">We Deployment & Manage</h3>
+            {/* <img src="/banners/development-illustration.jpg" alt="" className="mix-blend-darken" /> */}
             <p>
               Bringing your website to life with reliable and efficient deployment solutions. We ensure your site is
               securely launched, optimized for all devices, and ready to engage your audience instantly.
@@ -71,7 +83,8 @@ export default function HomePage() {
                 <div className="overflow-hidden w-[100%]">
                   <motion.img
                     whileHover={{ scale: 1.1, filter: "grayscale(0)" }}
-                    src={`https://picsum.photos/200/300?img${i}`}
+                    className="grayscale-0 md:grayscale-[.7]"
+                    src={`https://picsum.photos/200/${(i + 1) % 3 == 0 ? 3 : 2}00?img${i}`}
                   />
                 </div>
               );
